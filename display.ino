4e3dd089c97bdbe7cee0a5477cef47da37f2e77d
Onkar Bante <113540905+Onkar-Bante@users.noreply.github.com>
2023-10-26 15:51:15 +0530

    Delete display.ino

--- display.ino
+++
@@ -1,9 +0,0 @@
-
-void initializeDisplay() {
-  lcd.init();      // Initialize the LCD
-  lcd.backlight(); // Turn on the backlight
-  lcd.setCursor(0, 0); // Set cursor to the first line
-  lcd.print("  IFR Intern's"); // Print the first line of text
-  lcd.setCursor(0, 1); // Set cursor to the second line
-  lcd.print("   SPOT-MICRO"); // Print on the second line
-}
