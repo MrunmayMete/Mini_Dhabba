6992c00b017a58de9e9e0ea49b26d36564e1eabc
Onkar Bante <113540905+Onkar-Bante@users.noreply.github.com>
2023-10-26 15:51:00 +0530

    Delete microsec_to_angle.ino

--- microsec_to_angle.ino
+++
@@ -1,5 +0,0 @@
-void setServoAngle(int servoPin, int angle) {
-  // Map the desired angle (0 to 180) to the servo's actual pulse width (500 to 2400)
-  int pulseWidth = map(angle, 0, 180, 500, 2400);
-  servos[servoPin - 2].writeMicroseconds(pulseWidth);
-}
